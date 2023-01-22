8677b1afd1bb2d4750a4f9b59907555f87a3efef
Andrea Sequeira <sequeira.an@northeastern.edu>
2023-01-22 16:03:31 -0500

    Initial commit of lab-1

---
+++ src/Model/Person.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Nidhi Raghavendra
+ */
+public class Person {
+    
+    private String name;
+    Address address;
+    Address permanentAddress;
+    
+    Contact personalContact;
+    Contact officeContact;
+    
+    public Person() {
+        this.name = "";
+        this.address = new Address();
+        this.permanentAddress = new Address();
+        
+        this.officeContact = new Contact();
+        this.personalContact = new Contact();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getPermanentAddress() {
+        return permanentAddress;
+    }
+
+    public void setPermanentAddress(Address permanentAddress) {
+        this.permanentAddress = permanentAddress;
+    }
+
+    public Contact getPersonalContact() {
+        return personalContact;
+    }
+
+    public void setPersonalContact(Contact personalContact) {
+        this.personalContact = personalContact;
+    }
+
+    public Contact getOfficeContact() {
+        return officeContact;
+    }
+
+    public void setOfficeContact(Contact officeContact) {
+        this.officeContact = officeContact;
+    }
+    
+    
+}
